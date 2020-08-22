8b7826fddfd5bd901cf915458a5b759703b7486f
sc.lin <read0@DESKTOP-HDLMPH8>
2020-08-22 15:54:03 +0800

    finish multiple dim array testing

--- src/TestMultArrayOperate/Program.cs
+++ src/TestMultArrayOperate/Program.cs
@@ -10,7 +10,17 @@ namespace TestMultArrayOperate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow World");
+            int[,] test = new int[3, 4] 
+                            { { 1, 1, 1, 1},
+                              { 2, 2, 2, 2},
+                              { 3, 3, 3, 3} };
+
+            // this refer to https://stackoverflow.com/questions/4260207/how-do-you-get-the-width-and-height-of-a-multi-dimensional-array
+            Console.WriteLine($"using GetLength to get 1st dim:{test.GetLength(0)}");
+            Console.WriteLine($"using GetLength to get 2nd dim:{test.GetLength(1)}");
+
+            // test other method
+            Console.WriteLine($"rank: {test.Rank}"); // note: matrix's rank!
             Console.ReadLine();
         }
     }
