22ef0fcc481af4853053fdfa47e10b26c937a72c
bill <read0651@hotmail.com>
2020-11-13 17:11:34 +0800

    test

--- src/TestGetClassWith_GetType/Program.cs
+++ src/TestGetClassWith_GetType/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace TestGetClassWith_GetType
 {
+    /// <summary>
+    /// This project test  how to get all classes in specific namespace. 
+    /// And my goal is get class in three levels classes, 
+    /// e.g. get class in three namespace Algorithm, Algorithm.Category1 and Algorithm.Category1.SubCategory.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            // Test GetExcutingAssembly().GetTypes()
+            Type[] types = System.Reflection.Assembly.GetExecutingAssembly().GetTypes();
+            foreach (Type _type in types)
+            {
+                Console.WriteLine($"{_type.Name}, in{_type.UnderlyingSystemType.Namespace}");
+            }
+
+            // Test 
+            Console.WriteLine("\n");
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetAssembly(typeof(DateTime));
+            foreach (var exportedType in assembly.GetExportedTypes())
+            {
+                Console.WriteLine($"{exportedType.Name}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
