a8595c9e0dc5f0d551e7e536c2ac58bf7ce76d21
bill <read0651@hotmail.com>
2020-11-19 16:07:24 +0800

    TestOverloadLogicOperator NEW: create project.

---
+++ src/TestOverloadLogicOperator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOverloadLogicOperator
+{
+    public sealed class SelectedAlgorithmType
+    {
+        public SelectedAlgorithmType()
+        { 
+        }
+
+        #region Properties
+        public int Proiority { set; get; }
+        public bool IsUsed { set; get; }
+        public string Algorithm { set; get; }
+        public string Name { set; get; }
+        #endregion Properties
+
+        public static bool operator ==(SelectedAlgorithmType left, SelectedAlgorithmType right)
+        {
+            return left.Algorithm.Equals(right); 
+        }
+
+        public static bool operator !=(SelectedAlgorithmType left, SelectedAlgorithmType right)
+        {
+            return !left.Algorithm.Equals(right); 
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool isEqual = false;
+            if (obj != null &&
+                obj.GetType() == typeof(SelectedAlgorithmType))
+            { 
+
+            }
+            return base.Equals(obj);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
