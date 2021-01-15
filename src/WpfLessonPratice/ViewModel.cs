f4ead973893cd88bb3116e03a30dcc5d724b6bf3
sc.lin <read0@DESKTOP-HDLMPH8>
2021-01-16 00:08:11 +0800

    2021.01.09 sc.lin New: Create WpfLessonPractice to pratice WPF lesson exercise in 01.09, 01.16, 01.23. In 01.09, create BMI calculator to understand WPF and MVVM.

---
+++ src/WpfLessonPratice/ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLessonPratice
+{
+    public class ViewModel : NotifyPropertyBase
+    {
+        public ViewModel()
+        {
+        }
+
+        private double mHeight;
+        public double Height
+        {
+            //set { mHeight = value; }
+            set { SetProperty<double>(ref mHeight, value, nameof(Height)); }
+            get { return mHeight; }
+        }
+
+        private double mWeight;
+        public double Weight
+        {
+            //set { mHeight = value; }
+            set { SetProperty<double>(ref mWeight, value, nameof(Weight)); }
+            get { return mWeight; }
+        }
+
+        private double mBmi;
+        public double BMI
+        {
+            set { SetProperty<double>(ref mBmi, value, nameof(BMI)); }
+            get { return mBmi; }
+        }
+
+        private System.Windows.Input.ICommand mChangeCommand;
+        public System.Windows.Input.ICommand Command
+        {
+            get
+            {
+                if (mChangeCommand == null)
+                {
+                    mChangeCommand = new RelayCommand((x) =>  BMI = Weight / Height / Height);
+                }
+                return mChangeCommand;
+            }
+        }
+    }
+}
