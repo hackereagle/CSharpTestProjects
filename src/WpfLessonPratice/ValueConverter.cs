e42fa582f97dd6e69d226f99daad9d3c351b7b66
sc.lin <read0@DESKTOP-HDLMPH8>
2021-01-16 20:49:33 +0800

    2021.01.16 sc.lin add new practice: relative binding

---
+++ src/WpfLessonPratice/ValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace WpfLessonPratice
+{
+    [ValueConversion(typeof(double), typeof(Brush))]
+    class NumberColorConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return new SolidColorBrush(Colors.Blue);
+            var v = (double)value;
+            if (v < 100)
+            { return new SolidColorBrush(Colors.Blue); }
+            else if (v > 1000)
+            { return new SolidColorBrush(Colors.Coral); }
+            else
+            { return new SolidColorBrush(Colors.Green); }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
