a33a6612ba2fe0d191a1d9bcb7673a56f04becb3
sc.lin <read0@DESKTOP-HDLMPH8>
2021-03-01 22:35:45 +0800

    2021.02.28 sc.lin New: Add existed project TestThreadPoolTimeoutEvent and TestTaskTimeout. The couple project are in order to test that use Task and ThreadPool to implement algorithm execution module requirements: 1. End waitting when all algorithms are complete. 2. End waitting if more than one algorithm timeout.

---
+++ src/TestAlgExeModFinishAndTimeout/TestThreadPoolTimeoutEvent/Algorithm2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestThreadPoolTimeoutEvent
+{
+    class Algorithm2 : AlgBase
+    {
+        public Algorithm2(string name) : base(name)
+        { }
+
+        protected override bool GeneratorInspectMethod()
+        {
+            Console.WriteLine($"{System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff")}: ThreadID:{System.Threading.Thread.CurrentThread.ManagedThreadId}, run Algorithm2, name:{mName}");
+            System.Threading.Thread.Sleep(4000);
+            Console.WriteLine($"{System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff")}: ThreadID:{System.Threading.Thread.CurrentThread.ManagedThreadId}, name:{mName} finish!");
+            return true;
+        }
+
+    }
+}
---
+++ src/TestAlgExeModFinishAndTimeout/TestThreadPoolTimeoutEvent/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestThreadPoolTimeoutEvent
+{
+    class Program
+    {
+        static System.Threading.ManualResetEvent eventX = new System.Threading.ManualResetEvent(false);
+        static int mAmountOfItem = -1;
+        static object mMutex = new object();
+
+        static void ExecuteAlgorithm(object data)
+        {
+            object[] package;
+            if (data is object[])
+                package = (object[])data;
+            else
+                return;
+
+            AlgBase algo = (AlgBase)package[0];
+            int test = (int)package[1];
+
+            algo.Inspect();
+
+            lock (mMutex)
+            {
+                mAmountOfItem--;
+                if (mAmountOfItem == -1)
+                    eventX.Set();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // setting threadpool
+            System.Threading.ThreadPool.SetMinThreads(1, 1);
+            System.Threading.ThreadPool.SetMaxThreads(4, 4);
+
+            List<AlgBase> algorithms = new List<AlgBase>();
+            algorithms.Add(new Algorithm1("test1"));
+            algorithms.Add(new Algorithm2("test2"));
+            algorithms.Add(new Algorithm3("test3"));
+            algorithms.Add(new Algorithm2("test4"));
+
+            eventX.Reset();
+            System.Threading.WaitCallback workUnit = new System.Threading.WaitCallback(ExecuteAlgorithm);
+            foreach (AlgBase alg in algorithms)
+            {
+                lock (mMutex)
+                {
+                    mAmountOfItem++;
+                }
+                object[] param = new object[2] { alg, 0};
+                System.Threading.ThreadPool.QueueUserWorkItem(workUnit, param);
+            }
+
+            if (eventX.WaitOne(20000, true) == false)
+            {
+                Console.WriteLine($"{System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff")}: Have one timeout!");
+            }
+            else
+            {
+                Console.WriteLine("all back");
+            }
+
+            eventX.Reset();
+            Console.ReadLine();
+        }
+    }
+}
