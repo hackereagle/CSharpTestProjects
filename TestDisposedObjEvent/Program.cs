724e8be83033194702fe92fc720078004cb763c3
世智 林 <read0651@hotmail.com>
2022-07-12 13:42:18 +0800

    New: Added TestDisposedObjEvent project to test whether event working after subscribing object disposed.

---
+++ TestDisposedObjEvent/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDisposedObjEvent
+{
+    public delegate void delSomeEvent();
+    public class Observated : IDisposable
+    {
+        public Observated()
+        { }
+
+        public void Dispose()
+        { 
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public event delSomeEvent SomeEvent;
+        public void DoSomething()
+        {
+            if (SomeEvent != null)
+                SomeEvent.Invoke();
+        }
+
+        private bool _disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            { 
+            }
+
+            _disposed = true;
+        }
+    }
+
+
+    public class Test : IDisposable
+    {
+        public Test()
+        { }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void SomeMethod()
+        {
+            Console.WriteLine("yo");
+        }
+
+        private bool _disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            { 
+            }
+
+            _disposed = true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observated a = new Observated();
+            Test b = new Test();
+
+            a.SomeEvent += b.SomeMethod;
+
+            a.DoSomething();
+            b.Dispose();
+            a.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+}
