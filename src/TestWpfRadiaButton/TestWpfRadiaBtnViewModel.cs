504fbfa76545cc77b7e53ce002ebaf76b9bedab9
sclin <read0651@hotmail.com>
2024-06-14 22:51:03 +0800

    Finished view model.

--- src/TestWpfRadiaButton/TestWpfRadiaBtnViewModel.cs
+++ src/TestWpfRadiaButton/TestWpfRadiaBtnViewModel.cs
@@ -14,14 +14,39 @@ namespace TestWpfRadiaButton
         public bool IsChoice1Checked
         {
             get => _isChoice1Checked;
-            set => SetProperty(ref _isChoice1Checked, value);
+            set
+            { 
+                SetProperty(ref _isChoice1Checked, value);
+                _result = $"Choice1 = {_isChoice1Checked}, Choice2 = {_isChoice2Checked}, Choice3 = {_isChoice3Checked}";
+                OnPropertyChanged(nameof(Result));
+            }
         }
 
-        private bool _isChoice2Checked = true;
+        private bool _isChoice2Checked = false;
         public bool IsChoice2Checked
         {
             get => _isChoice2Checked;
-            set => SetProperty(ref _isChoice2Checked, value);
+            set
+            {
+                SetProperty(ref _isChoice2Checked, value);
+                _result = $"Choice1 = {_isChoice1Checked}, Choice2 = {_isChoice2Checked}, Choice3 = {_isChoice3Checked}";
+                OnPropertyChanged(nameof(Result));
+            }
         }
+
+        private bool _isChoice3Checked = false;
+        public bool IsChoice3Checked
+        {
+            get => _isChoice3Checked;
+            set
+            {
+                SetProperty(ref _isChoice3Checked, value);
+                _result = $"Choice1 = {_isChoice1Checked}, Choice2 = {_isChoice2Checked}, Choice3 = {_isChoice3Checked}";
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        private string _result = string.Empty;
+        public string Result => _result;
     }
 }
