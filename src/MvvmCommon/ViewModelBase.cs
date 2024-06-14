672a189aebc61799ec035d6346ce312086420b06
sclin <read0651@hotmail.com>
2024-06-14 19:09:09 +0800

    Added testing data/control template project and testing WPF radia button project.

---
+++ src/MvvmCommon/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MvvmCommon
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected void SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                storage = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+    }
+}
